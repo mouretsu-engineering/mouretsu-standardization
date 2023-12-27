b3e41064edab21c9984f8c2150f3214902eccb27
杉本悠哉 <y.sugimot358@gmail.com>
2023-12-27 20:08:28 +0900

    Googleログイン機能の作成

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// pagePropsにsessionを含むAppPropsの型を拡張します。
+type ExtendedAppProps = AppProps & {
+  pageProps: {
+    session?: any; // sessionの型を指定します。詳細な型定義が必要な場合は適宜変更してください。
+  };
+};
+
+export default function App({ Component, pageProps }: ExtendedAppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
